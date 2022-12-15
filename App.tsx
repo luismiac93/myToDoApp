8c505bd672c3a3d4a5457da2b4f757a50dcb97a4
Luismi <luismiac93@outlook.com>
2022-12-15 13:20:12 +1100

    basis of the my to do app project

---
+++ App.tsx
@@ -0,0 +1,16 @@
+
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>hELLO</Text>
+    </NavigationContainer>
+  );
+};
+
+export default App;
