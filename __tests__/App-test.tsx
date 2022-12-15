b6bd5c0f4c8f59d99d180c6016ea9e0e8663d371
Luismi <luismiac93@outlook.com>
2022-12-15 16:18:54 +1100

    basic test implementation for components

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,11 +4,38 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import {Header} from '../src/components/Header';
+import {TaskContext} from '../src/context';
+import {Priority} from '../src/components/Priority';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('Header render correctly', () => {
+  const tree = renderer
+    .create(
+      <TaskContext.Provider
+        value={{
+          tasks: [],
+          completedTask: () => {},
+          createTask: () => {},
+          deleteTask: () => {},
+          orderBy: () => {},
+        }}>
+        <Header />
+      </TaskContext.Provider>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Priority render correctly', () => {
+  const tree = renderer
+    .create(
+      <Priority
+        task={{id: '', priority: 0, task: 'do some test', status: false}}
+      />,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
