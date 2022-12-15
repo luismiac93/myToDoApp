489defa3ea34aec57288ba4142b4e5e4a5b3c595
Luismi <luismiac93@outlook.com>
2022-12-15 13:29:49 +1100

    basic navigation pages

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {HomeScreen} from '../screens/HomeScreen';
+import {DetailScreen} from '../screens/DetailScreen';
+
+const Stack = createStackNavigator();
+
+export const Navigation = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: '#19196F',
+        },
+      }}>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="DetailScreen" component={DetailScreen} />
+    </Stack.Navigator>
+  );
+};
